77ff4a52ec2dd3f5d0a363b29637eaef558d133a
Thaina0806 <thaina.parisotto@escola.pr.gov.br>
2026-09-22 11:43:26 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,28 +8,29 @@ const simbolos = '!@#%*?,.';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
-const forcaSenha = document.querySelector('forca');
+const forcaSenha = document.querySelector('.forca');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho() {
-    if(tamanhoSenha>1) {
-        //tamanhoSenha = tamanhoSenha-1;
-        tamanhoSenha--;
-    }
-    numeroSenha.textContent = tamanhoSenha;
-    geraSenha()
+if(tamanhoSenha>1) {
+//tamanhoSenha = tamanhoSenha-1;
+tamanhoSenha--;
+}
+numeroSenha.textContent = tamanhoSenha;
+geraSenha()
 }
 
 function aumentaTamanho() {
-    if(tamanhoSenha<20) {
-        //tamanhoSenha = tamanhoSenha+1;
-        tamanhoSenha++;
-    }
-    numeroSenha.textContent = tamanhoSenha;
-    geraSenha();
+if(tamanhoSenha<20) {
+//tamanhoSenha = tamanhoSenha+1;
+tamanhoSenha++;
 }
+numeroSenha.textContent = tamanhoSenha;
+geraSenha();
+}
+
 for(i=0; i < checkbox.length; i++){
 checkbox[i].onclick = geraSenha;
 }
@@ -60,4 +61,56 @@ senha = senha + alfabeto[numeroAleatorio];
 
 campoSenha.value = senha;
 classificaSenha(alfabeto.length);
+}
+function classificaSenha(tamanhoAlfabeto) {
+
+
+let entropia =
+tamanhoSenha * Math.log2(tamanhoAlfabeto);
+
+
+
+console.log(entropia);
+
+
+
+forcaSenha.classList.remove('fraca', 'media', 'forte');
+
+
+if (entropia >
+57) {
+
+
+
+forcaSenha.classList.add('forte');
+
+
+} else if
+(entropia > 35 && entropia < 57) {
+
+
+
+forcaSenha.classList.add('media');
+
+
+} else if
+(entropia <= 35) {
+
+
+
+forcaSenha.classList.add('fraca');
+
+
+}
+
+
+const
+valorEntropia = document.querySelector('.entropia');
+
+
+
+valorEntropia.textContent = "Um computador pode levar até " +
+Math.floor(2 ** entropia / (100e6 * 60 * 60 * 24)) +
+" dias para descobrir essa senha.";
+
 }
